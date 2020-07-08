d8c3017c70f1123c96a072b30bc1759fbddfd160
Stas Zarudensky <szarudensky@gmail.com>
2020-07-09 02:16:54 +0300

    Add test task

---
+++ js/main.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+  $('.menu__icon').click(function(){
+    $('.topmenu').slideToggle();
+  });
+  $('.submenu__link-first').click(function(){
+    $('.menu__second').slideToggle();
+  });
+  $('.submenu__link-second').click(function(){
+    $('.menu__third').slideToggle();
+  });
+  
+  $('.main__slider').slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear',
+    autoplay: true,
+    arrows: false,
+    pauseOnFocus: false,
+    pauseOnHover: false
+  });
+
+});
